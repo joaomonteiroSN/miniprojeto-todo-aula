67ab318b076f045f6eef4b71343e162770b88415
joao monteiro <70484799+joaomonteiroSN@users.noreply.github.com>
2025-04-19 21:18:02 -0300

    criacao da funcao de listagem e busca das tarefas

--- app.js
+++ app.js
@@ -8,3 +8,24 @@ function fecharModal() {
     criarTarefa.classList.remove("active")
 }
 
+function buscarTarefas() {
+    fetch("http://localhost:3000/tarefas")
+        .then(res => res.json())
+        .then(res => {
+            inserirTarefas(res)
+        })
+} buscarTarefas()
+
+function inserirTarefas(listaDeTarefas) {
+    if (listaDeTarefas.length > 0) {
+        lista.innerHTML = ""
+        listaDeTarefas.map(tarefa => {
+            lista.innerHTML +=
+                `<li>
+                    <h5>${tarefa.titulo}</h5>
+                    <p>${tarefa.descricao}</p>
+                    <box-icon size="sd" type='solid' name='trash'></box-icon>
+                </li>`
+        })
+    }
+} 
